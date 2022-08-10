364522aa283d3fea9f578b386ebdd6fc96250dbb
Kanpinar <kanpinar@hotmail.com>
2022-08-10 15:00:49 +0300

    test

--- src/app/image/image.component.ts
+++ src/app/image/image.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormGroup} from "@angular/forms";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-image',
-  templateUrl: './image.component.html',
-  styleUrls: ['./image.component.css']
+    selector: 'app-image',
+    templateUrl: './image.component.html',
+    styleUrls: ['./image.component.css']
 })
 export class ImageComponent implements OnInit {
+    @Input() multiple = false;
+    @Input() uFormGroup: FormGroup;
+    @Input() uFormControlName = 'file';
 
-  constructor() { }
+    base64Image:string;
+    constructor(public domSanitizer: DomSanitizer) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    getImageBase64(): any {
+
+            return this.domSanitizer.bypassSecurityTrustResourceUrl(this.base64Image);
+
+        return '';
+    }
+
+    onFileSelected($event: Event): void {
+        console.log($event)
+        const fileInput = $event.target as HTMLInputElement;
+        const reader = new FileReader();
+        reader.onload = (x): void => {
+            this.base64Image = reader.result.toString();
+            if (this.uFormGroup)
+                this.uFormGroup.get(this.uFormControlName).setValue(reader.result);
+            this.getImageBase64();
+        };
+        reader.readAsDataURL(fileInput.files[0]);
+    }
 }
